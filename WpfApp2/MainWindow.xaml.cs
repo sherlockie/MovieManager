1c3e7a8ba8c5a33f02daae094b8781f1708ac00b
sherlock <sherlock@DESKTOP-R7F8CIE>
2022-07-29 13:52:20 +0800

    添加项目文件。

---
+++ WpfApp2/MainWindow.xaml.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp2
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            GetAllFilms();
+            MoviesList.ItemsSource = Films;
+        }
+
+        private void Window_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            DragMove();
+        }
+
+        // 获取所有影片文件名字
+        private static void GetAllFilms(string path= "C:\\file\\films")
+        {
+            DirectoryInfo rootPath = new DirectoryInfo(path);
+            FileSystemInfo[] filesArray = rootPath.GetFileSystemInfos();
+            foreach (var item in filesArray)
+            {
+                if (item.Attributes == FileAttributes.Directory)
+                {
+                    GetAllFilms(item.FullName);
+                }
+                else
+                {
+                    MovieInfo movieInfo = new MovieInfo()
+                    {
+                        FileName = item.FullName
+                    };
+                    Films.Add(movieInfo);
+                }
+            }
+        }
+
+        // 所有影片文件列表
+        private static List<MovieInfo> Films = new();
+
+        private void addNewfilm_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("WIP.");
+        }
+    }
+
+    class MovieInfo
+    {
+        public string? Name { get; set; }
+        public string? FileName { get; set; }
+    }
+}
