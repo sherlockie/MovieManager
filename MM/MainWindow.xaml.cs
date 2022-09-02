25c7d3bb7da3cd2be81f6f2923eb98162523da23
褚正全 <chuzhengquan@live.com>
2022-09-02 19:17:53 +0800

    init

---
+++ MM/MainWindow.xaml.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace MM
+{
+    /// <summary>
+    /// MainWindow.xaml 的交互逻辑
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            homePage hp = new homePage();
+            mainFrame.Content = hp;
+        }
+
+        private void Window_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            this.DragMove();
+        }
+
+        private void ExitButton_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+
+        private void MenuButton_Click(object sender, RoutedEventArgs e)
+        {
+            popMenu.IsOpen = true;
+        }
+
+        private void AboutButton_Click(object sender, RoutedEventArgs e)
+        {
+            popAbout.IsOpen = true;
+        }
+
+        private void SearchButton_Click(object sender, RoutedEventArgs e)
+        {
+            searchPage sp = new searchPage();
+            mainFrame.Content = sp;
+        }
+
+        private void HomeButton_Click(object sender, RoutedEventArgs e)
+        {
+            homePage hp = new homePage();
+            mainFrame.Content = hp;
+        }
+    }
+}
---
+++ MM/homePage.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace MM
+{
+    /// <summary>
+    /// homePage.xaml 的交互逻辑
+    /// </summary>
+    public partial class homePage : Page
+    {
+        public homePage()
+        {
+            InitializeComponent();
+        }
+    }
+}
