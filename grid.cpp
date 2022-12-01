1dd4a0967cf3ca90fa554ce7e83a002df9832737
chuzhengquan <chuzhengquan@live.com>
2022-12-01 17:36:40 +0800

    新增库页面网格显示；

--- grid.cpp
+++ grid.cpp
@@ -1,7 +1,57 @@
 #include "grid.h"
+#include "pagecontrol.h"
+
+#include <QLabel>
+#include <QDebug>
 
 grid::grid(QWidget *parent)
     : QWidget{parent}
 {
+    int total = 100;
+    int pageSize = 8;
+    mainLayout = new QVBoxLayout();
+    // 第一页所有电影网格展示
+    initMovies();
+    // 页面控制控件
+    pageControl *pagecontrol = new pageControl();
+    pagecontrol->initPage(total, 1, pageSize);
+    connect(pagecontrol, SIGNAL(pageChanged(int)), this, SLOT(showPage(int)));
+    // 开始布局
+    mainLayout->addLayout(allItemsLayout);
+    mainLayout->addWidget(pagecontrol, 0, Qt::AlignCenter);
+    this->setLayout(mainLayout);
+}
 
+void grid::initMovies()
+{
+    // 初始化所有电影，但不设置数据
+    int cols = 4;  // 每行四个
+    allItemsLayout = new QGridLayout();
+    allItemsLayout->setSpacing(20);
+    for(int i = 0; i < 8; ++i)
+    {
+        int row = i / cols;
+        int col = i % cols;
+        movieLayout = new QVBoxLayout();
+        movieLayout->setAlignment(Qt::AlignCenter);
+        QPushButton *movieButton = new QPushButton();
+        movieButton->setMinimumHeight(300);
+        // 海报默认
+        QLabel *poster = new QLabel();
+        poster->setPixmap(QPixmap(QString(":/default/Resources/%1.jpg").arg(i%7+1)));
+        QLabel *name = new QLabel("默认电影名");
+        movieLayout->addWidget(poster);
+        movieLayout->addWidget(name);
+
+        movieButton->setLayout(movieLayout);
+        movieList << movieButton;
+
+        allItemsLayout->addWidget(movieButton, row, col);
+    }
+}
+
+void grid::showPage(int p)
+{
+    qDebug() << QString("当前页码%1").arg(p);
+    // 按照所给页数p从数据库中分页查询结果，重新置于model中显示
 }
--- grid.h
+++ grid.h
@@ -2,14 +2,27 @@
 #define GRID_H
 
 #include <QWidget>
+#include <QVBoxLayout>
+#include <QGridLayout>
+#include <QPushButton>
 
 class grid : public QWidget
 {
     Q_OBJECT
 public:
     explicit grid(QWidget *parent = nullptr);
+    void initMovies();
+
+public slots:
+    void showPage(int p);
 
 signals:
+
+private:
+    QVBoxLayout *mainLayout;  // 整体竖直布局，所有电影一块，页面控制器一块
+    QGridLayout *allItemsLayout;  // 所有电影以网格布局
+    QVBoxLayout *movieLayout;  // 每个电影包括海报和名字，竖直布局
+    QList<QPushButton *> movieList;
 
 };
 
